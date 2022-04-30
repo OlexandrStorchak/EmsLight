beb12ce9fb78757406be9d4b56b2a8501339e9ee
Oleksandr <alexsstorchak@gmail.com>
2022-04-30 18:29:31 +0300

    add role column to admin user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_30_114217) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_30_145921) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -38,6 +38,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_30_114217) do
     t.datetime "updated_at", null: false
     t.string "first_name", default: ""
     t.string "last_name", default: ""
+    t.string "role"
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
