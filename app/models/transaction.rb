8c06fb91847975b60566d371cc79327cd8c2d005
Oleksandr <alexsstorchak@gmail.com>
2022-05-18 10:15:07 +0300

    add optional fields(agent,category,project)

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,9 +1,9 @@
 class Transaction < ApplicationRecord
   belongs_to :admin_user, primary_key: "id", foreign_key: "author_id"
   belongs_to :account
-  belongs_to :agent
-  belongs_to :category
-  belongs_to :project
+  belongs_to :agent, optional: true
+  belongs_to :category, optional: true
+  belongs_to :project, optional: true
 
   validates :description, presence: { message: "description cant be blank" }
   validates :occured_at, presence: true
